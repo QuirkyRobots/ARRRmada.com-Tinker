f9f21bf71c521d577037fed152f7954533242f15
QR <clickbox100@gmail.com>
2024-09-11 23:49:26 +1000

    Corrections

--- assets/js/merchant.js
+++ assets/js/merchant.js
@@ -130,8 +130,6 @@ function generateID() {
         a.className = 'tag-btn';
         a.dataset.tagId = tag.id;
         a.innerText = tag.name;
-        a.innerText = tag.fdate;
-        a.innerText = tag.featured;
         merchantTags.appendChild(a);
       });
     }
